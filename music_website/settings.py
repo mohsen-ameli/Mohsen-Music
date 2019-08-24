ca6c5ab3bb922a5f18242fbbdd6d5c6e5f1fc18c
Mohsen Ameli <mohsen.ameli83@gmail.com>
2019-08-24 16:17:29 -0400

    secret key and vars

--- music_website/settings.py
+++ music_website/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1y#efz4*jeaib-9qvj^@e2&d@bh9cgm-kv5lwgbxvx+*e+jxnd'
+SECRET_KEY = os.environ.get('SECRET_KEY_MUSIC_WEBSITE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
